4e222cd7424d3396b79db47cfd637aa42838accf
selindimitri@gmail.com <selindimitri@gmail.com>
2022-10-16 20:57:11 +0300

    Add DI to the projects

--- Backend/WildForest.Api/Program.cs
+++ Backend/WildForest.Api/Program.cs
@@ -1,6 +1,11 @@
+using WildForest.Application;
+using WildForest.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
-
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
 }
 
 var app = builder.Build();
