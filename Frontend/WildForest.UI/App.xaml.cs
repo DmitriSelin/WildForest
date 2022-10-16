d23f0cc410559aa77763c95d3f08960d116e3917
selindimitri@gmail.com <selindimitri@gmail.com>
2022-10-16 20:37:41 +0300

    Create project structure

---
+++ Frontend/WildForest.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WildForest.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Frontend/WildForest.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WildForest.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
