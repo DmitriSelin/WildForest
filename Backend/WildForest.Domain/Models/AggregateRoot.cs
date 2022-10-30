633b779267d7d73d9f3dff8f2d83dc015c6179a2
selindimitri@gmail.com <selindimitri@gmail.com>
2022-10-30 16:11:04 +0300

    Create base classes for Entity, ValueObject, AggregateRoot

---
+++ Backend/WildForest.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace WildForest.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
---
+++ Backend/WildForest.Domain/Models/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WildForest.Domain.Models
+{
+    public abstract class Entity<TId> : IEquatable<TId>
+        where TId : notnull
+    {
+        public TId Id { get; set; } 
+
+        protected Entity(TId id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Entity<TId> entity && Id.Equals(entity.Id);
+        }
+
+        public static bool operator ==(Entity<TId> left, Entity<TId> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<TId> left, Entity<TId> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(TId? other)
+        {
+            return Equals((object?)other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
