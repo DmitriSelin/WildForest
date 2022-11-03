4942ecbd8293f3869d1b9f31e1981c5557dc3763
selindimitri@gmail.com <selindimitri@gmail.com>
2022-11-04 00:07:47 +0300

    Create WeatherForecastController

---
+++ Backend/WildForest.Api/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WildForest.Api.Controllers
+{
+    [Route("api/weather/forecast")]
+    [Authorize]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet("{cityId}")]
+        public async Task<IActionResult> GetTodayWeather([FromQuery] Guid cityId)
+        {
+            // Find city in db
+
+            // Get weather:
+            // Temperature, description,
+            // humidity, pressure,
+            // wind, type of weather,
+            // cloudness, storm
+
+            return Ok();
+        }
+    }
+}
