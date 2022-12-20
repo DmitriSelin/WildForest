8f2805a0d967668498b12261c6490866c2d7d78f
selindimitri@gmail.com <selindimitri@gmail.com>
2022-12-21 00:16:49 +0300

    Add json file with cities and their location

--- Modules/WildForest.Console/Program.cs
+++ Modules/WildForest.Console/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿StartDialog();
+
+static void StartDialog()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("\n Welcome to WildForest console application!");
+    Console.WriteLine("\n Please, enter file's name of someName.json \n");
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write(" ");
+    string fileName = Console.Read().ToString();
+    Console.ForegroundColor = ConsoleColor.Green; 
+}
