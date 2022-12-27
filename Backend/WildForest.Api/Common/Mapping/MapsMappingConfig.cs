ede0a872afcd9cb3f3d24cf9c26a7918972315b6
selindimitri@gmail.com <selindimitri@gmail.com>
2022-12-27 20:45:54 +0300

    Redone mapping classes

--- Backend/WildForest.Api/Common/Mapping/MapsMappingConfig.cs
+++ Backend/WildForest.Api/Common/Mapping/MapsMappingConfig.cs
@@ -1,14 +1,16 @@
 ﻿using Mapster;
 using WildForest.Application.Maps.Queries.GetCitiesList;
+using WildForest.Application.Maps.Queries.GetCountriesList;
 using WildForest.Contracts.Maps;
 
 namespace WildForest.Api.Common.Mapping
 {
-    public class CityMappingConfig : IRegister
+    public class MapsMappingConfig : IRegister
     {
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<CityQuery, CityResponse>();
+            config.NewConfig<CountryQuery, CountryResponse>();
         }
     }
 }
