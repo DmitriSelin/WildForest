a8c9d38566b0271d9326d763683f3eada792a124
selindimitri@gmail.com <selindimitri@gmail.com>
2023-01-12 23:59:06 +0300

    Delete rubbish

--- Modules/WildForest.Console/Countries/Services/CountryService.cs
+++ Modules/WildForest.Console/Countries/Services/CountryService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using WildForest.Domain.Countries.Entities;
-using WildForest.Domain.Countries.ValueObjects;
 using WildForest.Infrastructure.Context;
 
 namespace WildForest.Console.Countries.Services
@@ -26,16 +25,5 @@ namespace WildForest.Console.Countries.Services
             await context.Countries.AddAsync(country);
             await context.SaveChangesAsync();
         }
-
-        //public static CountryId GetCountryIdByName(string countryName)
-        //{
-        //    var optionsBuilder = new DbContextOptionsBuilder<WildForestDbContext>();
-        //    var options = optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgreSQL")).Options;
-        //    var context = new WildForestDbContext(options);
-
-        //    var country = context.Countries.FirstOrDefault(x => x.Name == countryName);
-
-        //    return country.Id;
-        //}
     }
 }
