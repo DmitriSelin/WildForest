1b783d82a23250a3364c9b998010e339ee000974
selindimitri@gmail.com <selindimitri@gmail.com>
2023-02-04 20:06:32 +0300

    Redone applicationUrl

--- Backend/WildForest.Api/Controllers/TestController.cs
+++ Backend/WildForest.Api/Controllers/TestController.cs
@@ -1,6 +1,27 @@
-﻿namespace WildForest.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using WildForest.Api.Services.Http.Weather;
+
+namespace WildForest.Api.Controllers
 {
-    public class TestController
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
     {
+        private readonly IWeatherForecastHttpClient _httpClient;
+
+        public TestController(IWeatherForecastHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TestGetWeather()
+        {
+            Guid cityId = Guid.Parse("c964e75d-d3d7-4da8-bc77-2faa55e3e512");
+
+            await _httpClient.GetWeatherForecastAsync(cityId);
+
+            return Ok();
+        }
     }
 }
--- Backend/WildForest.Api/Services/Http/Weather/WeatherForecastHttpClient.cs
+++ Backend/WildForest.Api/Services/Http/Weather/WeatherForecastHttpClient.cs
@@ -1,6 +1,7 @@
 ﻿using WildForest.Application.Common.Interfaces.Persistence;
 using WildForest.Application.Weather.Common;
 using WildForest.Domain.Cities.ValueObjects;
+using WildForest.Infrastructure.Common.Extensions;
 
 namespace WildForest.Api.Services.Http.Weather
 {
@@ -38,12 +39,11 @@ namespace WildForest.Api.Services.Http.Weather
                 throw new ArgumentNullException(nameof(city));
             }
 
-            string lat = city.Location.Latitude.ToString();
-            string lon = city.Location.Longitude.ToString();
-            string units = "metric";
+            string lat = city.Location.Latitude.RemoveCommaByPeriodToString();
+            string lon = city.Location.Longitude.RemoveCommaByPeriodToString();
             string? appid = _configuration["WeatherForecast:ApiKey"];
 
-            var url = $"?lat={lat}&lon={lon}&units={units}&appid={appid}";
+            var url = $"?lat={lat}&lon={lon}&units=metric&appid={appid}";
 
             string response = await _httpClient.GetStringAsync(url);
 
