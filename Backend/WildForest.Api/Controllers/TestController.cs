13e6dc1c621023b2ad71d3a84b85fb7f4734cc59
selindimitri@gmail.com <selindimitri@gmail.com>
2023-03-24 21:11:38 +0300

    Delete rubbish

--- Backend/WildForest.Api/Controllers/TestController.cs
+++ Backend/WildForest.Api/Controllers/TestController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WildForest.Api.Services.Http.Weather;
-using WildForest.Domain.Cities.ValueObjects;
-using WildForest.Infrastructure.Persistence.Context;
+
 
 namespace WildForest.Api.Controllers
 {
@@ -10,21 +7,9 @@ namespace WildForest.Api.Controllers
     [Route("test")]
     public class TestController : ControllerBase
     {
-        private readonly WeatherForecastHttpClient _context;
-
-        public TestController(WeatherForecastHttpClient context)
+        public TestController()
         {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var cityId = CityId.Create();
-
-            _context.GetWeatherForecastAsync(cityId);
-
-            return Ok();
+            
         }
     }
 }
