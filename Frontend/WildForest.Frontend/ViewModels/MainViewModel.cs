2db36e2331401cd2b3b3df17a5ca87f3dcc60ad8
selindimitri@gmail.com <selindimitri@gmail.com>
2023-05-04 00:40:39 +0300

    Create base window logic

---
+++ Frontend/WildForest.Frontend/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WildForest.Frontend.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        #region Commands
+
+        #region CloseAppCommand
+
+        public ICommand CloseAppCommand { get; }
+
+        private void CloseApp()
+            => App.Current.Shutdown();
+
+        #endregion
+
+        #region CollapseAppCommand
+
+        public ICommand CollapseAppCommand { get; }
+
+        private void CollapseApp()
+        {
+            App.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        #endregion
+
+        #region CollapseAppInWindowCommand
+
+        public ICommand CollapseAppInWindowCommand { get; }
+
+        private void CollapseAppInWindow()
+        {
+            var window = App.Current.MainWindow;
+
+            if (window.WindowState == WindowState.Normal)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainViewModel()
+        {
+            CloseAppCommand = new RelayCommand(CloseApp);
+            CollapseAppCommand = new RelayCommand(CollapseApp);
+            CollapseAppInWindowCommand = new RelayCommand(CollapseAppInWindow);
+        }
+    }
+}
--- Frontend/WildForest.Frontend/Views/MainView.xaml.cs
+++ Frontend/WildForest.Frontend/Views/MainView.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace WildForest.Frontend.Views;
 
 public partial class MainView : Window
 {
     public MainView() => InitializeComponent();
+
+    private void MoveWindow(object sender, MouseButtonEventArgs e)
+        => DragMove();
 }
