ec97b497102ecc5413aa7e1b635c125ec019d5eb
selindimitri@gmail.com <selindimitri@gmail.com>
2023-05-31 00:57:27 +0300

    Redone logic with htttp-get query

--- Frontend/WildForest.Frontend/Services/Weather/WeatherService.cs
+++ Frontend/WildForest.Frontend/Services/Weather/WeatherService.cs
@@ -7,43 +7,36 @@ using WildForest.Frontend.Common;
 using WildForest.Frontend.Contracts.Authentication;
 using WildForest.Frontend.Contracts.Weather;
 using WildForest.Frontend.Services.Weather.Interfaces;
-using WildForest.Frontend.Services.Weather.JsonConverters;
 
-namespace WildForest.Frontend.Services.Weather
+namespace WildForest.Frontend.Services.Weather;
+
+public sealed class WeatherService : IWeatherService
 {
-    public class WeatherService : IWeatherService
+    private readonly HttpClient _httpClient;
+
+    public WeatherService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
+        _httpClient = httpClient;
+    }
 
-        public WeatherService(HttpClient httpClient)
+    public async Task<WeatherResponseBase> GetTodayWeatherForecastAsync(string token)
+    {
+        var forecastDate = $"{DateTime.Now.Year}.{DateTime.Now.Month}.{DateTime.Now.Day}";
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _httpClient.GetAsync($"{ApiItemKeys.BaseUrl}/weather/forecast/homeCity/{forecastDate}");
+
+        string body = await response.Content.ReadAsStringAsync();
+
+        if (response.IsSuccessStatusCode)
         {
-            _httpClient = httpClient;
+            var weatherForecast = JsonSerializer.Deserialize<WeatherForecastVm>(body);
+            return new(weatherForecast, (int)response.StatusCode, null);
         }
 
-        public async Task<WeatherResponseBase> GetTodayWeatherForecastAsync(string token)
-        {
-            var date = DateOnly.FromDateTime(DateTime.Now.Date);
-
-            var options = new JsonSerializerOptions();
-            options.Converters.Add(new DateOnlyConverter());
-
-            var forecastDate = JsonSerializer.Serialize(date, options);
-
-            _httpClient.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Bearer", token);
-
-            var response = await _httpClient.GetAsync($"{ApiItemKeys.BaseUrl}/weather/forecast/homeCity/{forecastDate}");
-
-            string body = await response.Content.ReadAsStringAsync();
-
-            if (response.IsSuccessStatusCode)
-            {
-                var weatherForecast = JsonSerializer.Deserialize<WeatherForecastVm>(body);
-                return new(weatherForecast, (int)response.StatusCode, null);
-            }
-
-            var badResponse = JsonSerializer.Deserialize<BadResponse>(body);
-            return new(null, badResponse!.Status, badResponse.Title);
-        }
+        var badResponse = JsonSerializer.Deserialize<BadResponse>(body);
+        return new(null, badResponse!.Status, badResponse.Title);
     }
 }
