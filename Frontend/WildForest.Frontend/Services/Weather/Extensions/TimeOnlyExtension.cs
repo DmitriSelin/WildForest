f1de403137d4453ec0db60bc0832faec254368a3
selindimitri@gmail.com <selindimitri@gmail.com>
2023-06-02 01:32:04 +0300

    Add bindings to weather view

---
+++ Frontend/WildForest.Frontend/Services/Weather/Extensions/TimeOnlyExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WildForest.Frontend.Services.Weather.Extensions;
+
+internal static class TimeOnlyExtension
+{
+    internal static TimeOnly PutIntoTimeInterval(this TimeOnly currentTime)
+    {
+        TimeOnly[] timeIntervals = new TimeOnly[]
+        {
+            new TimeOnly(0, 0), new TimeOnly(3, 0),
+            new TimeOnly(6, 0), new TimeOnly(9, 0),
+            new TimeOnly(12, 0), new TimeOnly(15, 0),
+            new TimeOnly(18, 0), new TimeOnly(21, 0)
+        };
+
+        TimeOnly interval;
+
+        for (int i = 0; i < timeIntervals.Length; i++)
+        {
+            interval = timeIntervals[i];
+
+            if (currentTime < interval)
+            {
+                if (i == 0)
+                {
+                    throw new ArgumentException("Invalid time");
+                }
+                
+                return timeIntervals[i - 1];
+            }
+        }
+
+        return timeIntervals[timeIntervals.Length - 1];
+    }
+}
--- Frontend/WildForest.Frontend/ViewModels/WeatherViewModel.cs
+++ Frontend/WildForest.Frontend/ViewModels/WeatherViewModel.cs
@@ -1,14 +1,113 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WildForest.Frontend.Contracts.Weather.Models;
+using WildForest.Frontend.Services.Weather.Extensions;
 using WildForest.Frontend.Services.Weather.Interfaces;
 
 namespace WildForest.Frontend.ViewModels
 {
-    internal class WeatherViewModel : ObservableObject
+    internal partial class WeatherViewModel : ObservableObject
     {
         private readonly IWeatherService _weatherService;
+
+        private WeatherForecastDto currentWeatherForecast = null!;
+
+        #region Properties
+
+        [ObservableProperty]
+        private string windSpeed;
+
+        [ObservableProperty]
+        private string direction;
+
+        [ObservableProperty]
+        private string windGust;
+
+        [ObservableProperty]
+        private string pressure;
+
+        [ObservableProperty]
+        private string humidity;
+
+        [ObservableProperty]
+        private string precipitationProbability;
+
+        [ObservableProperty]
+        private string cloudiness;
+
+        [ObservableProperty]
+        private string visibility;
+
+        [ObservableProperty]
+        private string precipitationVolume;
+
+        [ObservableProperty]
+        private string temperature;
+
+        [ObservableProperty]
+        private string temperatureFeelsLike;
+
+        [ObservableProperty]
+        private string weatherDescription;
+
+        [ObservableProperty]
+        private string firstTemperature;
+
+        [ObservableProperty]
+        private string secondTemperature;
+
+        [ObservableProperty]
+        private string thirdTemperature;
+
+        [ObservableProperty]
+        private string fourthTemperature;
+
+        [ObservableProperty]
+        private string fifthTemperature;
+
+        [ObservableProperty]
+        private string sixthTemperature;
+
+        [ObservableProperty]
+        private string seventhTemperature;
+
+        [ObservableProperty]
+        private string eighthTemperature;
+
+        private void FillObservableProperties()
+        {
+            WindSpeed = currentWeatherForecast.Wind.Speed.ToString();
+            Direction = currentWeatherForecast.Wind.Direction.ToString();
+            WindGust = currentWeatherForecast.Wind.Gust.ToString();
+            Pressure = currentWeatherForecast.Pressure.ToString();
+            Humidity = currentWeatherForecast.Humidity.ToString();
+            PrecipitationProbability = currentWeatherForecast.PrecipitationProbability.ToString();
+            Cloudiness = currentWeatherForecast.Cloudiness.ToString();
+            Visibility = currentWeatherForecast.Visibility.ToString();
+            PrecipitationVolume = currentWeatherForecast.PrecipitationVolume.ToString();
+            Temperature = currentWeatherForecast.Temperature.Value.ToString();
+            TemperatureFeelsLike = currentWeatherForecast.Temperature.ValueFeelsLike.ToString();
+            WeatherDescription = currentWeatherForecast.WeatherDescription.Description;
+        }
+
+        private void FillTemperature(List<WeatherForecastDto> forecasts)
+        {
+            FirstTemperature = forecasts[0].Temperature.Value.ToString();
+            SecondTemperature = forecasts[1].Temperature.Value.ToString();
+            ThirdTemperature = forecasts[2].Temperature.Value.ToString();
+            FourthTemperature = forecasts[3].Temperature.Value.ToString();
+            FifthTemperature = forecasts[4].Temperature.Value.ToString();
+            SixthTemperature = forecasts[5].Temperature.Value.ToString();
+            SeventhTemperature = forecasts[6].Temperature.Value.ToString();
+            EighthTemperature = forecasts[7].Temperature.Value.ToString();
+        }
+
+        #endregion
 
         #region Token
 
@@ -38,7 +137,13 @@ namespace WildForest.Frontend.ViewModels
 
             if (response.WeatherForecast is not null)
             {
+                var currentHour = TimeOnly.FromTimeSpan(DateTime.Now.TimeOfDay)
+                    .PutIntoTimeInterval();
 
+                currentWeatherForecast = response.WeatherForecast.WeatherForecasts.FirstOrDefault(x => x.Time == currentHour)!;
+
+                FillObservableProperties();
+                FillTemperature(response.WeatherForecast.WeatherForecasts);
             }
             else
             {
