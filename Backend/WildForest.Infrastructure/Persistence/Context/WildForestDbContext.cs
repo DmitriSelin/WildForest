065416ff8c9fa0508e1104e654482eb180ef12a2
DmitriSelin <selindimitri@gmail.com>
2023-07-14 00:03:34 +0300

    Change DbContext

--- Backend/WildForest.Infrastructure/Persistence/Context/WildForestDbContext.cs
+++ Backend/WildForest.Infrastructure/Persistence/Context/WildForestDbContext.cs
@@ -4,19 +4,18 @@ using WildForest.Domain.Countries.Entities;
 using WildForest.Domain.Tokens.Entities;
 using WildForest.Domain.Users.Entities;
 using WildForest.Domain.Weather.Entities;
-using WildForest.Infrastructure.Persistence.Configurations;
 
 namespace WildForest.Infrastructure.Persistence.Context;
 
 public sealed class WildForestDbContext : DbContext
 {
-    public DbSet<User> Users => Set<User>();
-
-    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
-
     public DbSet<City> Cities => Set<City>();
 
     public DbSet<Country> Countries => Set<Country>();
+
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 
     public DbSet<ThreeHourWeatherForecast> ThreeHourWeatherForecasts => Set<ThreeHourWeatherForecast>();
 
@@ -24,8 +23,5 @@ public sealed class WildForestDbContext : DbContext
         => Database.EnsureCreated();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new UserConfiguration());
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WildForestDbContext).Assembly);
-    }
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(WildForestDbContext).Assembly);
 }
