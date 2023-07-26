6703d599dbf276d246fa6566db5a4a10433ae8c4
selindimitri@gmail.com <selindimitri@gmail.com>
2023-07-26 16:37:04 +0300

    Create base colors

--- Frontend/wildforest.ui/src/main.js
+++ Frontend/wildforest.ui/src/main.js
@@ -1,7 +1,8 @@
-import './styles/tailwind.css'
+import './styles/tailwind.css';
+import './styles/colors.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 let app = createApp(App);
 app.mount('#app');
