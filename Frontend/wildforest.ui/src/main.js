36595a58ecccdfa67a678be67462b018a4350554
selindimitri@gmail.com <selindimitri@gmail.com>
2023-10-02 23:15:56 +0300

    Create style for combobox

--- Frontend/wildforest.ui/src/main.js
+++ Frontend/wildforest.ui/src/main.js
@@ -5,7 +5,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/primevue.min.css';
-import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
 
 let app = createApp(App);
 app.use(router);
--- Frontend/wildforest.ui/src/views/WFRegister.vue
+++ Frontend/wildforest.ui/src/views/WFRegister.vue
@@ -2,6 +2,26 @@
 import WFInput from "@/components/inputs/WFInput.vue"
 import WFButton from "@/components/buttons/WFButton.vue"
 import WFEmptyLink from "@/components/buttons/WFEmptyLink.vue";
+import Dropdown from 'primevue/dropdown';
+
+import { ref } from "vue";
+
+const selectedCity = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'London1', code: 'LDN1' },
+    { name: 'London2', code: 'LDN2' },
+    { name: 'London3', code: 'LDN3' },
+    { name: 'London4', code: 'LDN4' },
+    { name: 'London5', code: 'LDN5' },
+    { name: 'London7', code: 'LDN7' },
+    { name: 'London8', code: 'LDN8' },
+    { name: 'London6', code: 'LDN6' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
 </script>
 
 <template>
@@ -15,6 +35,7 @@ import WFEmptyLink from "@/components/buttons/WFEmptyLink.vue";
                 <WFInput label="Name" name="name" placeholder="Input your name" />
                 <WFInput label="Surname" name="secondName" placeholder="Input your surname" />
                 <WFInput label="Email" name="email" placeholder="Input your email" />
+                <Dropdown v-model="selectedCity" editable :options="cities" optionLabel="name" placeholder="Select a City" class="combobox"/>
                 <WFInput label="Password" type="password" name="password" placeholder="Input your password" />
                 <WFInput label="Password" type="password" name="samePassword" placeholder="Input the same password" />
                 <div class="left-block-content-btn">
@@ -34,4 +55,5 @@ import WFEmptyLink from "@/components/buttons/WFEmptyLink.vue";
 
 <style lang="scss" scoped>
 @import "./styles/views.scss";
+@import "../components/styles/combobox.scss";
 </style>
