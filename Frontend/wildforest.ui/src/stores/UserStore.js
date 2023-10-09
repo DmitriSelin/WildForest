d9ccdbe9bc188bb3689b68f1f49b9dc0969bf3cb
selindimitri@gmail.com <selindimitri@gmail.com>
2023-10-09 23:12:11 +0300

    Create function for getting auth credentials

--- Frontend/wildforest.ui/src/stores/UserStore.js
+++ Frontend/wildforest.ui/src/stores/UserStore.js
@@ -4,21 +4,27 @@ import { url } from "@/infrastructure/urls/urlUtility"
 import ky from 'ky';
 
 export const useUserStore = defineStore("userStore", () => {
+    const authCredentials = ref({});
     const registerResponse = ref({});
-    const registerCredentials = ref({});
 
-    const getRegisterCredentials = async () => {
-        registerCredentials.value = await ky.get(`${url}auth/countries-languages`).json();
-    }
+    const getAuthCredentials = async () => {
+        try {
+            authCredentials.value = await ky.get(`${url}auth/countries-languages`).json();
+        }
+        catch (error) {
+            console.log("Server error");
+        }
+    };
 
     const register = async (request) => {
         const registerRequest = JSON.stringify(request);
-        registerResponse.value = await ky.post(`${url}auth/register`, {json: registerRequest}).json();
+        registerResponse.value = await ky.post(`${url}auth/register`, { json: registerRequest }).json();
     }
 
     return {
-        registerCredentials,
         registerResponse,
+        authCredentials,
+        getAuthCredentials,
         register
     };
 });
--- Frontend/wildforest.ui/src/views/WFAuthCredentials.vue
+++ Frontend/wildforest.ui/src/views/WFAuthCredentials.vue
@@ -1,26 +1,18 @@
 <script setup>
 import WFButton from "@/components/buttons/WFButton.vue"
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useUserStore } from "@/stores/UserStore";
 
+const selectedLanguage = ref();
+const selectedCountry = ref();
 const userStore = useUserStore();
 
-const selectedCity = ref();
-const cities = ref([
-    { name: 'New York', code: 'NY' },
-    { name: 'Rome', code: 'RM' },
-    { name: 'London', code: 'LDN' },
-    { name: 'London1', code: 'LDN1' },
-    { name: 'London2', code: 'LDN2' },
-    { name: 'London3', code: 'LDN3' },
-    { name: 'London4', code: 'LDN4' },
-    { name: 'London5', code: 'LDN5' },
-    { name: 'London7', code: 'LDN7' },
-    { name: 'London8', code: 'LDN8' },
-    { name: 'London6', code: 'LDN6' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Paris', code: 'PRS' }
-]);
+onMounted(async () => {
+    await userStore.getAuthCredentials();
+});
+
+const goToRegisterPage = () => {
+}
 </script>
 
 <template>
@@ -30,11 +22,11 @@ const cities = ref([
                 <img src="../assets/images/heart.svg" alt="heart" class="left-block-name-heart" />
                 <h1>Registration</h1>
             </div>
-            <form class="left-block-content">
-                <Dropdown v-model="selectedCity" editable :options="userStore.registerCredentials.languages"
-                    optionLabel="name" placeholder="Select language" class="combobox" />
-                <Dropdown v-model="selectedCity" editable :options="userStore.registerCredentials.countries" optionLabel="name" placeholder="Select a City"
-                    class="combobox" />
+            <form class="left-block-content" @submit.prevent="goToRegisterPage">
+                <Dropdown v-model="selectedLanguage" editable :options="userStore.authCredentials.languages"
+                    optionLabel="name" placeholder="Select a language" class="combobox" />
+                <Dropdown v-model="selectedCountry" editable :options="userStore.authCredentials.countries"
+                    optionLabel="name" placeholder="Select a country" class="combobox" />
                 <WFButton label="Next" size="large" />
             </form>
         </div>
