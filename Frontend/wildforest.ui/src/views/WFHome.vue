d09ba185da8f594775cbd131bc14693d4eb5fce2
selindimitri@gmail.com <selindimitri@gmail.com>
2023-10-22 22:02:31 +0300

    Basic markup

--- Frontend/wildforest.ui/src/views/WFHome.vue
+++ Frontend/wildforest.ui/src/views/WFHome.vue
@@ -3,7 +3,36 @@
 </script>
 
 <template>
-    <h1>Home</h1>
+    <main class="main">
+        <div class="main-weather-temperature">
+            <h1>CityName</h1>
+            <h2>Date</h2>
+            <img src="../assets/images/logo.ico" alt="weatherIcon">
+            <h2>Sunny</h2>
+        </div>
+        <div class="main-weather-data">
+            <div class="main-weather-data-wind">
+
+            </div>
+            <div class="main-weather-data-one">
+
+            </div>
+            <div class="main-weather-data-one">
+
+            </div>
+        </div>
+        <div class="main-weather-data">
+            <div class="main-weather-data-wind">
+
+            </div>
+            <div class="main-weather-data-one">
+
+            </div>
+            <div class="main-weather-data-one">
+                
+            </div>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
