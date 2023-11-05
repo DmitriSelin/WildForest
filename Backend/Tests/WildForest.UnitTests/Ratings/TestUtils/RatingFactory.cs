b5c35c139efc7190a9c1055c8b4cfcaba1d9d334
DmitriSelin <selindimitri@gmail.com>
2023-11-05 21:19:14 +0300

    Create structure for Rating unit tests

---
+++ Backend/Tests/WildForest.UnitTests/Ratings/TestUtils/RatingFactory.cs
@@ -0,0 +1,11 @@
+﻿using WildForest.Domain.Ratings;
+
+namespace WildForest.UnitTests.Ratings.TestUtils;
+
+public class RatingFactory
+{
+    public static Rating Create()
+    {
+        throw new Exception();//TODO: Create tests
+    }
+}
