782b3bea81f99ae38b4b1d2cd558dc295dec7c7c
selindimitri@gmail.com <selindimitri@gmail.com>
2023-11-13 22:23:03 +0300

    Add comment component

---
+++ src/WildForest.Api/wildforest.ui/src/components/comments/WFComment.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    comment: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <h1></h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
