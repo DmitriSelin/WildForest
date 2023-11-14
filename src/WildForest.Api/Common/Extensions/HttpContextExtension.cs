069bd88cfedfc8f6a411c6438a758c60bb6fc1df
selindimitri@gmail.com <selindimitri@gmail.com>
2023-11-14 22:58:00 +0300

    Redone logic of parsing id from jwt token

--- src/WildForest.Api/Common/Extensions/HttpContextExtension.cs
+++ src/WildForest.Api/Common/Extensions/HttpContextExtension.cs
@@ -1,4 +1,6 @@
+using ErrorOr;
 using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace WildForest.Api.Common.Extensions;
 
@@ -12,10 +14,25 @@ public static class HttpContextExtension
             return context.Connection.RemoteIpAddress!.MapToIPv4().ToString();
     }
 
-    public static StringValues GetJwtTokenFromAuthHeader(this HttpContext context)
+    public static ErrorOr<Guid> GetUserIdFromAuthHeader(this HttpContext context)
     {
-        context.Request.Headers.TryGetValue("Authorization", out StringValues token);
+        context.Request.Headers.TryGetValue("Authorization", out StringValues bearer);
 
-        return token;
+        var userId = Guid.Empty;
+
+        if (bearer.Any())
+        {
+            string? token = bearer[0]?.Split(" ")[1];
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwt = tokenHandler.ReadJwtToken(token);
+
+            userId = Guid.Parse(jwt.Claims.First(c => c.Type.Equals("sub")).Value);
+        }
+
+        if (userId == Guid.Empty)
+            return Domain.Common.Errors.Errors.Authentication.InvalidAuthorizationHeader;
+
+        return userId;
     }
 }
