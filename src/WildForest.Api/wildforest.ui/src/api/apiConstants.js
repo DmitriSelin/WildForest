5a7a4cb6a2d66f6cc602d01d02cbf42f19b6aa88
selindimitri@gmail.com <selindimitri@gmail.com>
2023-12-22 00:11:04 +0300

    Create class and constants for http requests

---
+++ src/WildForest.Api/wildforest.ui/src/api/apiConstants.js
@@ -0,0 +1,12 @@
+/*Result of request*/
+export const SUCCESS = "success";
+export const ERROR = "error";
+/*HTTP request methods*/
+export const GET = "GET";
+export const HEAD = "HEAD";
+export const POST = "POST";
+export const PUT = "PUT";
+export const PATCH = "PATCH";
+export const DELETE = "DELETE";
+/*HTTP response status codes*/
+export const UNAUTHORIZED = 401;
