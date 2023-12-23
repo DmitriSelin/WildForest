846ad31fbd48816597462065a33c98992a020acb
selindimitri@gmail.com <selindimitri@gmail.com>
2023-12-23 15:40:51 +0300

    Redone logic with weather requests

--- src/WildForest.Api/wildforest.ui/src/weather/requests/weatherRequests.js
+++
@@ -1,6 +0,0 @@
-import { useUserStore } from "@/stores/UserStore"
-
-export async function getHomeWeatherForecast() {
-    const userStore = useUserStore();
-    const currentDate = new Date().toJSON().slice(0, 10);
-}
---
+++ src/WildForest.Api/wildforest.ui/src/weather/weatherRequests.js
@@ -0,0 +1,11 @@
+import { Api } from "@/api/api";
+
+export class WeatherRequests {
+    constructor (api = new Api()) {
+        this.api = api;
+    }
+
+    getHomeWeatherForecasts() {
+        
+    }
+}
