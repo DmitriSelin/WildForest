a7363cdb2b8968c762f540ff4c8ebd770266198c
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-06 23:35:53 +0300

    Create new migration

---
+++ src/WildForest.Infrastructure/Migrations/20240106203419_RevokeDateMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WildForest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevokeDateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RevokedDate",
+                table: "RefreshTokens",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RevokedDate",
+                table: "RefreshTokens",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
