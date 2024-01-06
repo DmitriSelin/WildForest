c464de0b4200cc832e4dd8b0f9fa60465855f21c
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-06 23:38:12 +0300

    Add constants for Toast component

---
+++ src/WildForest.Api/wildforest.ui/src/infrastructure/components/toasts/toastConstants.js
@@ -0,0 +1,8 @@
+//Color of toast
+export const SUCCESS_SEVERITY = "success";
+export const INFO_SEVERITY = "info";
+export const WARN_SEVERITY = "warn";
+export const ERROR_SEVERITY = "error";
+//Lifetime of toast
+export const STANDARD_LIFE = 5_000;
+export const LONG_LIFE = 10_000;
