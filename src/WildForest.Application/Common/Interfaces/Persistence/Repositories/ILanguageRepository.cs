9ebd546b84c90aebbbea03ba0b76a8596755660e
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-15 23:40:17 +0300

    Add new method

--- src/WildForest.Application/Common/Interfaces/Persistence/Repositories/ILanguageRepository.cs
+++ src/WildForest.Application/Common/Interfaces/Persistence/Repositories/ILanguageRepository.cs
@@ -1,8 +1,11 @@
 using WildForest.Domain.Languages.Entities;
+using WildForest.Domain.Languages.ValueObjects;
 
 namespace WildForest.Application.Common.Interfaces.Persistence.Repositories;
 
 public interface ILanguageRepository
 {
     Task<IEnumerable<Language>> GetAllLanguagesAsync();
+
+    Task<Language?> GetLanguageByIdAsync(LanguageId languageId);
 }
--- src/WildForest.Infrastructure/Persistence/Repositories/LanguageRepository.cs
+++ src/WildForest.Infrastructure/Persistence/Repositories/LanguageRepository.cs
@@ -1,6 +1,8 @@
+using System.Reflection.Metadata.Ecma335;
 using Microsoft.EntityFrameworkCore;
 using WildForest.Application.Common.Interfaces.Persistence.Repositories;
 using WildForest.Domain.Languages.Entities;
+using WildForest.Domain.Languages.ValueObjects;
 using WildForest.Infrastructure.Persistence.Context;
 
 namespace WildForest.Infrastructure.Persistence.Repositories;
@@ -16,4 +18,7 @@ public sealed class LanguageRepository : ILanguageRepository
 
     public async Task<IEnumerable<Language>> GetAllLanguagesAsync()
         => await _context.Languages.ToListAsync();
+
+    public async Task<Language?> GetLanguageByIdAsync(LanguageId languageId)
+        => await _context.Languages.FirstOrDefaultAsync(x => x.Id == languageId);
 }
