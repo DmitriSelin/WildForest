002cb66ed1a59d737518d73b74004dbd4d65496b
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-15 23:41:23 +0300

    Redone method for updating profile

--- src/WildForest.Application/Authentication/Commands/Profile/ProfileService.cs
+++ src/WildForest.Application/Authentication/Commands/Profile/ProfileService.cs
@@ -7,6 +7,9 @@ using WildForest.Domain.Users.Entities;
 using WildForest.Domain.Cities.ValueObjects;
 using WildForest.Domain.Languages.ValueObjects;
 using WildForest.Application.Common.Interfaces.Authentication;
+using WildForest.Domain.Tokens.Entities;
+using WildForest.Domain.Cities.Entities;
+using WildForest.Domain.Languages.Entities;
 
 namespace WildForest.Application.Authentication.Commands.Profile;
 
@@ -30,18 +33,26 @@ public sealed class ProfileService : IProfileService
     {
         var userId = UserId.Create(command.Id);
         var email = Email.Create(command.Email);
+        var cityId = CityId.Create(command.CityId);
+        var languageId = LanguageId.Create(command.LanguageId);
+        City? city = null;
+        Language? language = null;
+
         var user = await _unitOfWork.UserRepository.GetUserByEmailAsync(email);
 
         if (user is null || user.Id != userId || !user.Password.IsEqual(command.Password))
             return Errors.User.NotFound;
 
+        if (user.CityId != cityId)
+            city = await _unitOfWork.CityRepository.GetCityByIdAsync(cityId);
+
+        if (user.LanguageId != languageId)
+            language = await _unitOfWork.LanguageRepository.GetLanguageByIdAsync(languageId);
+
         var newUserCredentials = CreateUser(command);
-        user.Update(newUserCredentials);
+        user.Update(newUserCredentials, city, language);
 
-        var refreshToken = await _refreshTokenGenerator.GenerateTokenAsync(user.Id, command.IpAddress);
-
-        await _unitOfWork.RefreshTokenRepository.AddTokenAsync(refreshToken);
-        await _unitOfWork.RefreshTokenRepository.RemoveOldRefreshTokensByUserIdAsync(user.Id);
+        var refreshToken = await GetRefreshTokenAsync(user.Id, command.IpAddress);
         await _unitOfWork.SaveChangesAsync();
 
         string accessToken = _jwtTokenGenerator.GenerateToken(user);
@@ -56,7 +67,7 @@ public sealed class ProfileService : IProfileService
         var email = Email.Create(command.Email);
         Password password;
 
-        if (command.NewPassword is null)
+        if (string.IsNullOrWhiteSpace(command.NewPassword))
             password = Password.Create(command.Password);
         else
             password = Password.Create(command.NewPassword);
@@ -66,5 +77,13 @@ public sealed class ProfileService : IProfileService
 
         var user = User.CreateUser(firstName, lastName, email, password, cityId, languageId);
         return user;
+    }
+
+    private async Task<RefreshToken> GetRefreshTokenAsync(UserId userId, string ipAddress)
+    {
+        RefreshToken refreshToken = await _refreshTokenGenerator.GenerateTokenAsync(userId, ipAddress);
+        await _unitOfWork.RefreshTokenRepository.AddTokenAsync(refreshToken);
+        await _unitOfWork.RefreshTokenRepository.RemoveOldRefreshTokensByUserIdAsync(userId);
+        return refreshToken;
     }
 }
