016ec32b9b97dccbd2b1b8308d580936117413a5
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-26 00:17:20 +0300

    Refactoring

--- src/WildForest.Application/Authentication/Queries/Login/LoginQuery.cs
+++ src/WildForest.Application/Authentication/Queries/Login/LoginQuery.cs
@@ -1,4 +1,3 @@
-﻿namespace WildForest.Application.Authentication.Queries.LoginUser
-{
-    public sealed record LoginQuery(string Email, string Password, string IpAddress);
-}
+﻿namespace WildForest.Application.Authentication.Queries.LoginUser;
+
+public sealed record LoginQuery(string Email, string Password, string IpAddress);
