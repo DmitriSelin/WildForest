53b5622822f1f08d24ce820baa8787db93d39922
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-26 21:58:02 +0300

    Change mapping logic

--- src/WildForest.Application/Common/Mapping/MapMappingConfig.cs
+++ src/WildForest.Application/Common/Mapping/MapMappingConfig.cs
@@ -4,19 +4,18 @@ using WildForest.Application.Maps.Queries.GetCountriesList;
 using WildForest.Domain.Cities.Entities;
 using WildForest.Domain.Countries.Entities;
 
-namespace WildForest.Application.Common.Mapping
-{
-    public sealed class MapMappingConfig : IRegister
-    {
-        public void Register(TypeAdapterConfig config)
-        {
-            config.NewConfig<Country, CountryQuery>()
-                .Map(dest => dest.CountryId, src => src.Id.Value)
-                .Map(dest => dest.CountryName, src => src.Name.Value);
+namespace WildForest.Application.Common.Mapping;
 
-            config.NewConfig<City, CityQuery>()
-                .Map(dest => dest.CityId, src => src.Id.Value)
-                .Map(dest => dest.CityName, src => src.Name.Value);
-        }
+public sealed class MapMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Country, CountryQuery>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.Name, src => src.Name.Value);
+
+        config.NewConfig<City, CityQuery>()
+            .Map(dest => dest.Id, src => src.Id.Value)
+            .Map(dest => dest.Name, src => src.Name.Value);
     }
 }
