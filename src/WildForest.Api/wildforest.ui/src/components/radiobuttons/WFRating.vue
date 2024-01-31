8038c29a63bc08e58cb26c1d1c0fdd4b258151bd
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-31 23:29:04 +0300

    Add state with sessionStorage

--- src/WildForest.Api/wildforest.ui/src/components/radiobuttons/WFRating.vue
+++ src/WildForest.Api/wildforest.ui/src/components/radiobuttons/WFRating.vue
@@ -1,9 +1,11 @@
 <script setup>
 import WFRadiobutton from './WFRadiobutton.vue';
-import { ref, watch } from 'vue';
+import { getItemFromSessionStorage, setItemInSessionStorage } from "@/infrastructure/storage/storageUtils";
+import { ref, onMounted, watch } from 'vue';
 
 const up = '1';
 const down = '2';
+const upButtonCheckedStorageName = '';
 
 const props = defineProps({
     rating: {
@@ -16,14 +18,25 @@ const props = defineProps({
     }
 });
 
+const upButtonChecked = ref(false);
+
+onMounted(() => {
+    try {
+        upButtonChecked.value = getItemFromSessionStorage(upButtonCheckedStorageName);
+    }
+    catch (err) { /* empty */ }
+});
+
 const emits = defineEmits(['up', 'down']);
 const vote = ref('');
 
 watch(() => vote.value, (state) => {
     if (state === up) {
+        setItemInSessionStorage(upButtonCheckedStorageName, true);
         emits('up');
     }
     else if (state === down) {
+        setItemInSessionStorage(upButtonCheckedStorageName, false);
         emits('down');
     }
 });
@@ -31,9 +44,11 @@ watch(() => vote.value, (state) => {
 
 <template>
     <div class="container">
-        <WFRadiobutton name="vote" id="up" value="1" v-model:checkedValue="vote" icon="up" />
+        <WFRadiobutton name="vote" id="up" value="1" v-model:checkedValue="vote" icon="up"
+            :checked="upButtonChecked === true" />
         <span class="count">{{ rating }}</span>
-        <WFRadiobutton name="vote" id="down" value="2" v-model:checkedValue="vote" icon="down" />
+        <WFRadiobutton name="vote" id="down" value="2" v-model:checkedValue="vote" icon="down"
+            :checked="!upButtonChecked === false" />
     </div>
 </template>
 
