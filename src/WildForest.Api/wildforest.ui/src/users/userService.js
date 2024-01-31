73e1d9c4d03ab4f4807947f5e508bfa2a4bda6d1
selindimitri@gmail.com <selindimitri@gmail.com>
2024-01-31 23:29:28 +0300

    Write function for changing rating

--- src/WildForest.Api/wildforest.ui/src/users/userService.js
+++ src/WildForest.Api/wildforest.ui/src/users/userService.js
@@ -1,9 +1,9 @@
 import { Api } from "@/api/api";
 import { useUserStore } from "@/stores/UserStore";
-import { SUCCESS, ERROR, GET, PUT } from "@/api/apiConstants";
+import { SUCCESS, ERROR, GET, PUT, POST } from "@/api/apiConstants";
 import { RequestResult } from "@/api/requestResult";
 import { getItemFromSessionStorage, setItemInSessionStorage } from "@/infrastructure/storage/storageUtils";
-import { PROFILE_CREDENTIALS_STORAGE_NAME } from "./userConstants";
+import { PROFILE_CREDENTIALS_STORAGE_NAME, UP, DOWN } from "./userConstants";
 
 export class UserService {
     constructor(api = new Api()) {
@@ -40,6 +40,39 @@ export class UserService {
                 const badResponse = await requestResult.data.json();
                 return new RequestResult(ERROR, badResponse);
             }
+        }
+    }
+
+    async vote(voteResult, ratingId, voteId) {
+        const request = { ratingId: ratingId, userId: this.userStore.authResponse.id }
+        let requestResult;
+
+        if (voteId) {
+            const updationRequest = { ...request, id: voteId }
+            requestResult = await this.api.requestWithPayload("weather/rating/vote", PUT, updationRequest);
+
+            if (requestResult.result === SUCCESS) {
+                return requestResult;
+            }
+            else if (requestResult.result === ERROR) {
+                const badResponse = await requestResult.data.json();
+                return new RequestResult(ERROR, badResponse);
+            }
+        }
+
+        if (voteResult === UP) {
+            requestResult = await this.api.requestWithPayload("weather/rating/vote/up", POST, request);
+        }
+        else if (voteResult === DOWN) {
+            requestResult = await this.api.requestWithPayload("weather/rating/vote/down", POST, request);
+        }
+        
+        if (requestResult.result === SUCCESS) {
+            return requestResult;
+        }
+        else if (requestResult.result === ERROR) {
+            const badResponse = await requestResult.data.json();
+            return new RequestResult(ERROR, badResponse);
         }
     }
 }
