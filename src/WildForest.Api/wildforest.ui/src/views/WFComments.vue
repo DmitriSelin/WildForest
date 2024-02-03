25e72c81875b67263d624e32307724f97dee3201
selindimitri@gmail.com <selindimitri@gmail.com>
2024-02-03 12:12:33 +0300

    Add base template for comment page

--- src/WildForest.Api/wildforest.ui/src/views/WFComments.vue
+++ src/WildForest.Api/wildforest.ui/src/views/WFComments.vue
@@ -1,11 +1,42 @@
 <script setup>
+import WFInput from "@/components/inputs/WFInput.vue";
+import WFButton from '@/components/buttons/WFButton.vue'
+import { ref } from "vue";
 
+const comment = ref("");
+
+const sendComment = () => {
+    alert("");
+}
 </script>
 
 <template>
-    <h1>Comments</h1>
+    <main class="main">
+        <div class="comments">
+
+        </div>
+        <form @submit.prevent="sendComment" class="form">
+            <WFInput label="Comment" name="comment" placeholder="Input your comment" v-model:value="comment"
+                autocomplete="off" />
+            <WFButton label="Send" />
+        </form>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+.main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
 
+    .comments {
+        height: 90%;
+    }
+
+    .form {
+        display: flex;
+        gap: 10px;
+    }
+}
 </style>
