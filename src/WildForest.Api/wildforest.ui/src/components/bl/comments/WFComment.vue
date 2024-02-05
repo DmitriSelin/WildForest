74ec551f38fd4a4464334f5d5a582b2d8a46ac77
selindimitri@gmail.com <selindimitri@gmail.com>
2024-02-05 23:38:13 +0300

    Create comment component

---
+++ src/WildForest.Api/wildforest.ui/src/components/bl/comments/WFComment.vue
@@ -0,0 +1,64 @@
+<script setup>
+const props = defineProps({
+    comments: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="container">
+        <div class="comment" v-for="comment in comments" :key="comment.id">
+            <div class="credentials">
+                <img src="../../../assets/images/logo.ico" alt="userIcon">
+                <span>LastName FirstName</span>
+            </div>
+            <div class="text">
+                <span></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    height: 90%;
+    overflow: auto;
+    gap: 2vh;
+    padding: 0 5%;
+    margin: 2vh auto;
+    width: 100%;
+    align-items: center;
+
+    .comment {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        padding: 10px;
+        border-radius: 10px;
+
+        &:hover {
+            background-color: var(--second);
+        }
+
+        .credentials {
+            display: flex;
+            align-items: center;
+            gap: 2vh;
+        }
+
+        .text {
+            margin-left: 9%;
+
+            span {
+                word-wrap: break-word;
+                text-wrap: wrap;
+            }
+        }
+    }
+}
+</style>
