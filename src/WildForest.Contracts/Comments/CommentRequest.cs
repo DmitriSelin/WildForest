a59cf09b84088470fc48f4da65b01937beb023d5
selindimitri@gmail.com <selindimitri@gmail.com>
2024-02-13 22:50:46 +0300

    Add userId property

--- src/WildForest.Contracts/Comments/CommentRequest.cs
+++ src/WildForest.Contracts/Comments/CommentRequest.cs
@@ -2,12 +2,15 @@
 
 public class CommentRequest
 {
+    public Guid UserId { get; init; }
+
     public Guid WeatherForecastId { get; init; }
 
     public string Text { get; init; }
 
-    public CommentRequest(Guid weatherForecastId, string text)
+    public CommentRequest(Guid userId, Guid weatherForecastId, string text)
     {
+        UserId = userId;
         WeatherForecastId = weatherForecastId;
         Text = text;
     }
