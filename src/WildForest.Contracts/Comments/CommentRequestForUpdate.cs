9cb2f7df9a756eced879d4f343d54472db9ba8b6
selindimitri@gmail.com <selindimitri@gmail.com>
2024-02-14 23:37:28 +0300

    Add property to base constructor

--- src/WildForest.Contracts/Comments/CommentRequestForUpdate.cs
+++ src/WildForest.Contracts/Comments/CommentRequestForUpdate.cs
@@ -4,7 +4,7 @@ public sealed class CommentRequestForUpdate : CommentRequest
 {
     public Guid Id { get; init; }
 
-    public CommentRequestForUpdate(Guid id, Guid weatherForecastId, string text) : base(weatherForecastId, text)
+    public CommentRequestForUpdate(Guid id, Guid userId, Guid weatherForecastId, string text) : base(userId, weatherForecastId, text)
     {
         Id = id;
     }
