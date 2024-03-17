793267d3e4404e3b11b9d59423c2de2cf44e2913
selindimitri@gmail.com <selindimitri@gmail.com>
2024-03-17 15:36:58 +0300

    Change access modifier

--- src/WildForest.Infrastructure/Persistence/Repositories/Base/Repository.cs
+++ src/WildForest.Infrastructure/Persistence/Repositories/Base/Repository.cs
@@ -7,25 +7,25 @@ namespace WildForest.Infrastructure.Persistence.Repositories.Base;
 public class Repository<TEntity> : IRepository<TEntity>
     where TEntity : class, IEntity
 {
-    private readonly WildForestDbContext _context;
+    protected readonly WildForestDbContext Context;
 
     public Repository(WildForestDbContext context)
     {
-        _context = context;
+        Context = context;
     }
 
     public virtual async Task AddAsync(TEntity entity, CancellationToken token = default)
     {
-        await _context.AddAsync(entity, token);
+        await Context.AddAsync(entity, token);
     }
 
     public virtual void Update(TEntity entity)
     {
-        _context.Update(entity);
+        Context.Update(entity);
     }
 
     public virtual void Remove(TEntity entity)
     {
-        _context.Remove(entity);
+        Context.Remove(entity);
     }
 }
